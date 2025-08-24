e9df4ab2e5582d643997fa7c2554c09d1abe993e
Jhonier Javith Hernández <hernandezjhonier12@gmail.com>
2025-08-24 12:59:17 -0500

    Agrego programa Java de Actividad 2 Condicionales

---
+++ Actividad_2Condicionales/Actividad_2condicionales.java
@@ -0,0 +1,42 @@
+
+package actividad_2condicionales;
+
+import java.util.Scanner;
+
+
+public class Actividad_2condicionales {
+
+ 
+    public static void main(String[] args) {
+         Scanner leer = new Scanner(System.in);
+        
+        System.out.println("ingrese su nombre : ");
+        String nombre = leer.nextLine();
+        
+        System.out.println("ingrese su apellido : ");
+         String apellido = leer.nextLine();
+        
+        System.out.println("ingrese su edad : ");
+         int edad = leer.nextInt();
+          
+         leer.nextLine();
+           System.out.print("Ingrese su sexo (masculino/femenino): ");
+        String sexo = leer.nextLine();
+         
+         if (edad >= 18){
+             System.out.println(nombre+ " "  + apellido +  " es mayor de edad ");
+         }else{
+             System.out.println(nombre+ " "  + apellido +  " es menor de edad ");
+         }
+         
+       if (sexo.equalsIgnoreCase("masculino")){
+           System.out.println("es un hombre");
+       } else{
+           System.out.println("es una mujer");
+       }
+    }
+    
+}
+
+       
+
